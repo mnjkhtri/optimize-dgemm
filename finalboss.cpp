e00668ee1b04a82fb5a65168f7f4db73fc304637
Manoj Khatri <mk711325@gmail.com>
2022-11-11 17:23:00 +0545

    hit 32g

--- finalboss.cpp
+++ finalboss.cpp
@@ -10,8 +10,8 @@ static size_t N0;
 #define Y(i,j) Y[(i)*(N0)+(j)]
 #define Z(i,j) Z[(i)*(N0)+(j)]
 
-//64 bytes will fit into L2 cache (100 bytes was the limit), 32 (or even 16 to be safest) seems to do the best here
-#define BLOCK (16)
+//Three 36 size block of doubles fit into the L1 cache
+#define BLOCK (32)
 
 #define BLOCK_X (4)
 //Currently BLOCK_Y must be 8
@@ -27,7 +27,6 @@ static void kernel(double *X, double *Y, double *Z);
 //Function for computing Z[N*N] = Z[N*N] + X[N*N]*Y[N*N] 
 void finalboss(double *X, double *Y, double *Z, size_t N)
 {
-
     //True size of the matrix
     N0 = N;
     
@@ -45,35 +44,36 @@ void finalboss(double *X, double *Y, double *Z, size_t N)
     for (size_t k = 0; k < N; k += BLOCK)
     {
         //Each of these possibilities seem to have same performance
-        //kernel(&X(i,k), &Y(k,j), &Z(i,j));
-        //kernel(&X(k,i), &Y(i,j), &Z(k,j));
-        kernel(&X(i,j), &Y(j,k), &Z(i,k));
+        //kernel(&X(i,k), &Y(k,j), &Z(i,j));  //In the mid
+        //kernel(&X(k,i), &Y(i,j), &Z(k,j));  //This is the slowest
+        kernel(&X(i,j), &Y(j,k), &Z(i,k));  //[This does better]
     }
 }
 
 //PackX will be kernel's helper, packs X[4*BLOCK] into Xp (preserving the matrix property, will destroy submatricity)
 void PackX(double *X, double *Xp);
 
-//Packs Y[BLOCK*4] into Yp
+//Packs Y[BLOCK*8] into Yp
 void PackY(double *Y, double *Yp);
 
 void kernel(double *X, double *Y, double *Z)
 {
     //The block matrices are part of large entire matrices so memory accesses are scattered
-    double Xp[BLOCK*BLOCK];
-    double Yp[BLOCK*BLOCK];
+    alignas(64) double Xp[BLOCK*BLOCK];
+    alignas(64) double Yp[BLOCK*BLOCK];
  
     //Pack X[BLOCK*BLOCK] into Xp[BLOCK*BLOCK]; one loop preserves matricity of X[4*BLOCK] while destroys the submatricity
-    // for (size_t i = 0; i < BLOCK; i += 4)
-    // {
-    //     PackX(&X(i,0), &Xp[i*BLOCK]);
-    // }
+    for (size_t i = 0; i < BLOCK; i += 4)
+    {
+        PackX(&X(i,0), &Xp[i*BLOCK]);
+    }
+    
 
     //Pack Y[BLOCK*BLOCK] into Yp[BLOCK*BLOCK]; one loop preserves matricity of Y[BLOCK*4] while destroys the submatricity
-    // for (size_t j = 0; j < BLOCK; j += 4)
-    // {
-    //     PackY(&Y(0,j), &Yp[BLOCK*j]);
-    // }
+    for (size_t j = 0; j < BLOCK; j += 8)
+    {
+        PackY(&Y(0,j), &Yp[BLOCK*j]);
+    }
 
     //Loop over rows
     for (size_t i = 0; i < BLOCK; i += BLOCK_X)
@@ -81,7 +81,9 @@ void kernel(double *X, double *Y, double *Z)
         //Loop over column
         for (size_t j = 0; j < BLOCK; j += BLOCK_Y)
         {
-            finddot4by4(&X(i,0), &Y(0,j), &Z(i,j));
+        finddot4by4(&Xp[i*BLOCK], &Yp[j*BLOCK], &Z(i,j));
+        //finddot4by4(&Xp[i*BLOCK], &Y(0,j), &Z(i,j));
+        // finddot4by4(&X(i,0), &Y(0,j), &Z(i,j));
         }
     }
 }
@@ -120,9 +122,18 @@ void PackY(double *Y, double *Yp)
         *Yp++ = *yij_pntr++;
         *Yp++ = *yij_pntr++;
         *Yp++ = *yij_pntr++;
+        *Yp++ = *yij_pntr++;
+        *Yp++ = *yij_pntr++;
+        *Yp++ = *yij_pntr++;
+        *Yp++ = *yij_pntr++;
     }
 }
 
+#undef X
+#define X(i,j) X[i*BLOCK+j]
+
+#undef Y
+#define Y(i,j) Y[i*BLOCK_Y+j]
 
 //We have to multiply Z[BLOCK_X*BLOCK_Y] = X[BLOCK_Y*BLOCK] x Y[BLOCK*BLOCK_Y]
 static void finddot4by4(double *X, double *Y, double *Z)
