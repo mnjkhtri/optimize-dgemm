e315a553d46061c3e9befe1f38a8131bf67e6319
Manoj Khatri <mk711325@gmail.com>
2023-05-15 17:14:37 +0545

    Adjusted for column major

--- naive.cpp
+++ naive.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 
 #define X(i,j) X[(i)*N+(j)]
-#define Y(i,j) Y[(i)*N+(j)]
+#define Y(i,j) Y[(j)*N+(i)]
 #define Z(i,j) Z[(i)*N+(j)]
 
 void naive(double *X, double *Y, double *Z, int N)
 {
-    for (int p = 0; p < N; ++p)
+    for (int k = 0; k < N; ++k)
     {
         for (int i = 0; i < N; ++i)
         {
             for (int j = 0; j < N; ++j)
             {
-                Z(i,j) += X(i,p)*Y(p,j);
+                Z(i,j) += X(i,k)*Y(k,j);
             }
         }
     }
